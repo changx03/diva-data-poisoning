d3253ba2e22048ab77b85e2adb539a6b22804e3b
Luke Chang <luke.x.chang@gmail.com>
2021-11-24 11:40:07 +1300

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Adversarial Label Flipping Attack (ALFA) and Detect Using Complex Measures
+
+
---
+++ label_flip_revised/__init__.py
@@ -0,0 +1,4 @@
+from .alfa import alfa, solveLP, solveQP
+from .alfa_nn_v3 import solveLPNN, poison_attack
+
+__all__ = ['alfa', 'solveLP', 'solveQP', 'solveLPNN', 'poison_attack']
