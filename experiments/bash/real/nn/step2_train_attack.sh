89ec9db92acb56bc394362a389332e3ff8315264
Luke Chang <luke.x.chang@gmail.com>
2021-12-01 12:59:21 +1300

    [Real Step1] Select subset when more than 2000 samples

--- experiments/bash/real/nn/step2_train_attack.sh
+++ experiments/bash/real/nn/step2_train_attack.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 
 # Train SVM and generate Adversarial Label Flip Attack examples
-python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/abalone.csv -o ./data/output -t 0.2 -s 0.05
+python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/abalone_subset.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/australian.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/banknote.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/breastcancer.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/cardiotocography.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/cmc.csv -o ./data/output -t 0.2 -s 0.05
-python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/htru2.csv -o ./data/output -t 0.2 -s 0.05
-python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/phoneme.csv -o ./data/output -t 0.2 -s 0.05
-python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/ringnorm.csv -o ./data/output -t 0.2 -s 0.05
+python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/htru2_subset.csv -o ./data/output -t 0.2 -s 0.05
+python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/phoneme_subset.csv -o ./data/output -t 0.2 -s 0.05
+python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/ringnorm_subset.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/texture.csv -o ./data/output -t 0.2 -s 0.05
 python ./experiments/real/nn/step2_train_attack.py -f ./data/preprocessed/yeast.csv -o ./data/output -t 0.2 -s 0.05
