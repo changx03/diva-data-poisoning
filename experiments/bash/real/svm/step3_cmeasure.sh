5d5bd5b6d2ed620a2f7896d0ba4050da7b98e4dd
Luke Chang <luke.x.chang@gmail.com>
2021-12-02 13:09:11 +1300

    [Real SVM Step3] Added script

--- experiments/bash/real/svm/step3_cmeasure.sh
+++ experiments/bash/real/svm/step3_cmeasure.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
-# Compute C-Measures for each data set and save results as CSV file.
-
-Rscript ./experiments/real/svm/step3_cmeasure.R "./data/output/train" "./data/output/alfa" cmeasure_real_svm
+# Compute C-Measures for real datasets and save results as CSV file.
+# Parameters:
+# 1: clean path. 2: poison path. 3: output path. 4: output name
+mkdir ./results/real_svm
+Rscript ./experiments/real/svm/step3_cmeasure.R "./data/output/train/" "./data/output/alfa/" "./results/real_svm/" "real_svm"
