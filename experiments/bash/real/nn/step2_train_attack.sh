7f61f21d970aef6be7d6ce69ff85331e6767203d
Luke Chang <luke.x.chang@gmail.com>
2022-01-17 20:05:16 +1300

    adjust bash run orders

--- experiments/bash/real/nn/step2_train_attack.sh
+++ experiments/bash/real/nn/step2_train_attack.sh
@@ -6,7 +6,6 @@
 python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/australian_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
 # python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/banknote_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
 python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/breastcancer_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
-# python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/cardiotocography_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
 python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/cmc_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
 # python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/htru2_subset_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
 # python ./experiments/real/nn/step2_train_attack.py -f ./data/standard/phoneme_subset_std.csv -o ./data/output -t 0.2 -s 0.05 -m 0.41
