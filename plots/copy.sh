0819c48bc9d1a578b938d7802c469a0ad7a3b701
Luke Chang <luke.x.chang@gmail.com>
2022-01-19 18:25:07 +1300

    added script for plots

--- plots/copy.sh
+++ plots/copy.sh
@@ -4,7 +4,7 @@ cp ./results/synth_cor_acc_test_clean.pdf ../overleaf_cmeasures/images/
 cp ./results/synth_cor_acc_test_falfa_0.20.pdf ../overleaf_cmeasures/images/
 cp ./results/synth_cor_rate_falfa.pdf ../overleaf_cmeasures/images/
 
-cd ~/worksapce/overleaf_cmeasures
+cd ~/workspace/overleaf_cmeasures
 git pull
 git add -A
 git commit -m "update plots"
