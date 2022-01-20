190a69fc61dd9222bfe5359fc2bfe9898f787957
Luke Chang <luke.x.chang@gmail.com>
2022-01-20 18:41:33 +1300

    added ROC real repeated plot

--- plots/copy.sh
+++ plots/copy.sh
@@ -5,7 +5,7 @@ cp ./results/synth_cor_acc_test_falfa_0.20.pdf ../overleaf_cmeasures/images/
 cp ./results/synth_cor_rate_falfa.pdf ../overleaf_cmeasures/images/
 cp ./results/abalone_falfa_line.pdf ../overleaf_cmeasures/images/
 cp ./results/texture_falfa_line.pdf ../overleaf_cmeasures/images/
-cp ./results/roc_real.pdf ../overleaf_cmeasures/images/
+cp ./results/roc_real_repeated.pdf ../overleaf_cmeasures/images/
 cp ./results/roc_synth.pdf ../overleaf_cmeasures/images/
 
 
