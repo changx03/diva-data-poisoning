3336f7f340ebf0042c75d6c6e663190305c87927
Luke Chang <luke.x.chang@gmail.com>
2022-01-28 23:50:08 +1300

    updated script

--- bash/synth/step3_cm_rand.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-# Compute C-Measure for Synthetic data trained on neural network models 
-# 1: data path. 3: output path. 4: output name
-mkdir ./results/synth_rand
-echo "C-Measure for clean training data should be completed in SVM"
-echo "For poisoned data..."
-Rscript ./experiments/synth/step3_cmeasure.R "./data/synth/rand/" "./results/synth_rand/" "synth_rand"
