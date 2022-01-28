2006563648828be2100a6c6486c68b43526e46ef
Luke Chang <luke.x.chang@gmail.com>
2022-01-28 19:28:12 +1300

    fixed script

--- bash/synth/step1_data_gen.sh
+++ bash/synth/step1_data_gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Generate synthetic data
-python ./experiments/synth/step1_preprocessing.py -n 300 -f synth -s 2000
+python ./experiments/synth/step1_gen_data.py -n 300 -f synth -s 2000
