afef0c8a0e279e75311e0deb5e6625502e957a72
Luke Chang <luke.x.chang@gmail.com>
2022-02-02 16:47:47 +1300

    added correlation plot

--- plots_v2/copy.sh
+++ plots_v2/copy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cp ./results_plot/flfa_acc.pdf ../overleaf_cmeasures/images/
-# cp ./results/synth_cor_acc_test_clean.pdf ../overleaf_cmeasures/images/
-# cp ./results/synth_cor_acc_test_falfa_0.20.pdf ../overleaf_cmeasures/images/
-# cp ./results/synth_cor_rate_falfa.pdf ../overleaf_cmeasures/images/
+cp ./results_plot/synth_corr.pdf ../overleaf_cmeasures/images/
+
 # cp ./results/abalone_falfa_line.pdf ../overleaf_cmeasures/images/
 # cp ./results/texture_falfa_line.pdf ../overleaf_cmeasures/images/
 # cp ./results/roc_real_repeated.pdf ../overleaf_cmeasures/images/
