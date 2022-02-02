6a396b11b6206c86f57eb430337ed84ea9d6a8bf
Luke Chang <luke.x.chang@gmail.com>
2022-02-02 15:45:35 +1300

    added real_acc plot v2

---
+++ plots_v2/copy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp ./results_plot/flfa_acc.pdf ../overleaf_cmeasures/images/
+# cp ./results/synth_cor_acc_test_clean.pdf ../overleaf_cmeasures/images/
+# cp ./results/synth_cor_acc_test_falfa_0.20.pdf ../overleaf_cmeasures/images/
+# cp ./results/synth_cor_rate_falfa.pdf ../overleaf_cmeasures/images/
+# cp ./results/abalone_falfa_line.pdf ../overleaf_cmeasures/images/
+# cp ./results/texture_falfa_line.pdf ../overleaf_cmeasures/images/
+# cp ./results/roc_real_repeated.pdf ../overleaf_cmeasures/images/
+# cp ./results/roc_synth.pdf ../overleaf_cmeasures/images/
+# cp ./results/fake_acc.pdf ../overleaf_cmeasures/images/
+# cp ./results/synth_heatmap_up3.svg ../overleaf_cmeasures/images/
+# cp ./results/synth_heatmap_noise.svg ../overleaf_cmeasures/images/
+
+cd ~/workspace/overleaf_cmeasures
+git pull
+git add -A
+git commit -m "update plots"
+git push
